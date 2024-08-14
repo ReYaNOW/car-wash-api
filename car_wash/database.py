37ac021bab0616a26e8c0ce2889f7e9215760cc6
Sergei <reyangood@gmail.com>
2024-08-14 14:47:50 +0300

    add database.py

---
+++ car_wash/database.py
@@ -0,0 +1,26 @@
+from typing import AsyncGenerator
+
+from sqlalchemy.ext.asyncio import (
+    AsyncSession,
+    async_sessionmaker,
+    create_async_engine,
+)
+from sqlalchemy.orm import DeclarativeBase
+
+from car_wash.config import config
+
+db_url = config.database_url.unicode_string()
+
+
+async_url = db_url.replace('postgresql', 'postgresql+asyncpg')
+async_engine = create_async_engine(async_url)
+async_session_maker = async_sessionmaker(async_engine, expire_on_commit=False)
+
+
+class Base(DeclarativeBase):
+    pass
+
+
+async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
+    async with async_session_maker() as session:
+        yield session
