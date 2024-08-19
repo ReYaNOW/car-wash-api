af327c0d22e7321c1b7d6164c05bc722bbd07d5d
Sergei <reyangood@gmail.com>
2024-08-20 00:33:29 +0300

    add filling_db_var in config.py

--- car_wash/config.py
+++ car_wash/config.py
@@ -5,6 +5,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 class Config(BaseSettings):
     database_url: PostgresDsn
     debug: bool = False
+    filling_db: bool = False
 
     model_config = SettingsConfigDict(env_file='.env')
 
