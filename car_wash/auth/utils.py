f1e257d1686c90f7a2608f7434141a3c35f81f3c
Sergei <reyangood@gmail.com>
2024-09-01 16:45:22 +0300

    code refactoring in utils.py

--- car_wash/auth/utils.py
+++ car_wash/auth/utils.py
@@ -9,18 +9,18 @@ pwd_context = CryptContext(
 )
 
 
+def verify_pass(plain_password, hashed_password):
+    return pwd_context.verify(plain_password, hashed_password)
+
+
 async def verify_password_in_threadpool(plain_password, hashed_password):
     res = await run_in_threadpool(verify_pass, plain_password, hashed_password)
     return res
 
 
-def verify_pass(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
+def get_pass_hash(password):
+    return pwd_context.hash(password)
 
 
 async def get_pass_hash_in_threadpool(password):
     return await run_in_threadpool(get_pass_hash, password)
-
-
-def get_pass_hash(password):
-    return pwd_context.hash(password)
