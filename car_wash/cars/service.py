0bb81ef60d90062f26a7694f9f531904094247c0
Sergei <reyangood@gmail.com>
2024-09-01 03:33:58 +0300

    add create_user_car in UserCarService

--- car_wash/cars/service.py
+++ car_wash/cars/service.py
@@ -1,6 +1,15 @@
 from car_wash.cars.repository import UserCarRepository
+from car_wash.cars.schemas import UserCarCreate, UserCarCreateWithID
+from car_wash.users.models import User
 from car_wash.utils.service import GenericCRUDService
 
 
 class UserCarService(GenericCRUDService):
     repository = UserCarRepository
+
+    async def create_user_car(self, user: User, new_car: UserCarCreate):
+        new_car_with_id = UserCarCreateWithID(
+            **new_car.model_dump(), user_id=user.id
+        )
+        user_car_id = await UserCarService().create_entity(new_car_with_id)
+        return user_car_id
