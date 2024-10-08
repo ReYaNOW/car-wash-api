578635d8e4fe561a08969ae39aacfabbfa68fd13
Sergei <reyangood@gmail.com>
2024-10-08 18:15:23 +0300

    add field active to CarWash; fix relationships for CarWash; add price field for Booking; add CarWashPrice

--- car_wash/washes/models.py
+++ car_wash/washes/models.py
@@ -1,8 +1,9 @@
 from datetime import datetime, time
 
-from sqlalchemy import BigInteger, ForeignKey, func
+from sqlalchemy import BigInteger, ForeignKey, Numeric, UniqueConstraint, func
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 
+from car_wash.cars.models import CarBodyType
 from car_wash.database import Base
 from car_wash.users.models import User
 from car_wash.washes.locations.models import CarWashLocation
@@ -15,6 +16,7 @@ class CarWash(Base):
 
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str]
+    active: Mapped[bool]
 
     image_path: Mapped[str] = mapped_column(nullable=True)
     image_link: Mapped[str] = mapped_column(nullable=True)
@@ -30,6 +32,13 @@ class CarWash(Base):
 
 class Schedule(Base):
     __tablename__ = 'car_wash__schedule'
+    __table_args__ = (
+        UniqueConstraint(
+            'car_wash_id',
+            'day_of_week',
+            name='uix_car_wash_schedule___car_wash_id__day_of_week',
+        ),
+    )
 
     id: Mapped[int] = mapped_column(primary_key=True)
     car_wash_id: Mapped[int] = mapped_column(
@@ -41,7 +50,10 @@ class Schedule(Base):
     is_available: Mapped[bool] = mapped_column(default=True)
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
 
-    car_wash: Mapped['CarWash'] = relationship(back_populates='schedules')
+    # Relationships
+    car_wash: Mapped['CarWash'] = relationship(
+        'CarWash', back_populates='schedules'
+    )
 
 
 class Box(Base):
@@ -58,7 +70,10 @@ class Box(Base):
 
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
 
-    car_wash: Mapped['CarWash'] = relationship(back_populates='boxes')
+    # Relationships
+    car_wash: Mapped['CarWash'] = relationship(
+        'CarWash', back_populates='boxes'
+    )
     washer: Mapped['User'] = relationship('User')
     bookings: Mapped['Booking'] = relationship('Booking', back_populates='box')
 
@@ -73,6 +88,7 @@ class Booking(Base):
     box_id: Mapped[int] = mapped_column(
         ForeignKey(Box.id, ondelete='RESTRICT')
     )
+    price: Mapped[Numeric] = mapped_column(Numeric(10, 2), nullable=True)
 
     start_datetime: Mapped[datetime]
     end_datetime: Mapped[datetime]
@@ -80,5 +96,34 @@ class Booking(Base):
     is_exception: Mapped[bool] = mapped_column(default=False)
     created_at: Mapped[datetime] = mapped_column(server_default=func.now())
 
-    user: Mapped['User'] = relationship(back_populates='bookings')
-    box: Mapped['Box'] = relationship(back_populates='bookings')
+    # Relationships
+    user: Mapped['User'] = relationship('User', back_populates='bookings')
+    box: Mapped['Box'] = relationship('Box', back_populates='bookings')
+
+
+class CarWashPrice(Base):
+    __tablename__ = 'car_wash__price'
+    __table_args__ = (
+        UniqueConstraint(
+            'car_wash_id',
+            'body_type_id',
+            name='uix_car_wash_price___car_wash_id__body_type_id',
+        ),
+    )
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    car_wash_id: Mapped[int] = mapped_column(
+        ForeignKey(CarWash.id, ondelete='CASCADE')
+    )
+    body_type_id: Mapped[int] = mapped_column(
+        ForeignKey(CarBodyType.id, ondelete='CASCADE')
+    )
+    price: Mapped[Numeric] = mapped_column(Numeric(10, 2))
+
+    created_at: Mapped[datetime] = mapped_column(server_default=func.now())
+
+    # Relationships
+    car_wash: Mapped['CarWash'] = relationship(
+        'CarWash', back_populates='prices'
+    )
+    body_type: Mapped['CarBodyType'] = relationship('CarBodyType')
